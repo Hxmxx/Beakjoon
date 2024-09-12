9eabbb2431816e2077c6d61962585bc669c46690
조하민 <149176643+Hxmxx@users.noreply.github.com>
2024-09-12 14:10:54 +0900

    [Bronze IV] Title: 럭비 클럽, Time: 28 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2083. 럭비 클럽/럭비 클럽.py
@@ -0,0 +1,12 @@
+while True:
+    name, age, kg = input().split()
+    age = int(age)
+    kg = int(kg)
+
+    if name == '#' and age == 0 and kg == 0:
+        break
+
+    if age > 17 or kg >= 80:
+        print(f"{name} Senior")
+    else:
+        print(f"{name} Junior")
