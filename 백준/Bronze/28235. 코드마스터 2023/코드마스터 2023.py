47bd5ddea000f3b679c0e4806db44f0013b58ca1
조하민 <149176643+Hxmxx@users.noreply.github.com>
2024-10-04 16:17:50 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.py
@@ -0,0 +1,17 @@
+s = input()
+
+if s == "SONGDO" :
+    print("HIGHSCHOOL")
+    exit()
+
+elif s == "CODE" :
+    print("MASTER")
+    exit()
+
+elif s == "2023" :
+    print("0611")
+    exit()
+
+elif s == "ALGORITHM" :
+    print("CONTEST")
+    exit()
