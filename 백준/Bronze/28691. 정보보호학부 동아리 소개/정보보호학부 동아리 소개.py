8cb25b1fc91837698f2ff1718c32d2e90b481186
조하민 <149176643+Hxmxx@users.noreply.github.com>
2024-10-09 10:57:58 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 28 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.py
@@ -0,0 +1,14 @@
+# MatKor
+# WiCys
+# CyKor
+# AlKor
+# $clear
+
+
+s = input()
+
+if s == 'M' : print("MatKor")
+elif s == 'W' : print("WiCys")
+elif s == 'C' : print("CyKor")
+elif s == 'A' : print("AlKor")
+elif s == '$' : print("$clear")
