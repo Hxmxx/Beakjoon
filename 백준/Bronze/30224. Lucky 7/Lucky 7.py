2ac33e0b1a16269aac7b32c74555bb1572946ab9
조하민 <149176643+Hxmxx@users.noreply.github.com>
2024-11-17 21:19:14 +0900

    [Bronze V] Title: Lucky 7, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30224. Lucky 7/Lucky 7.py
@@ -0,0 +1,6 @@
+n = input()
+
+if '7' not in n and int(n) % 7 : print(0)
+elif '7' not in n and int(n) % 7 == 0 : print(1)
+elif '7' in n and int(n) % 7 : print(2)
+elif '7' in n and int(n) % 7 == 0 : print(3)
