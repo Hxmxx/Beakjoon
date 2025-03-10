0972a6718c8ad9962d4577bdbfb433fc5d6d1696
조하민 <149176643+Hxmxx@users.noreply.github.com>
2025-03-11 08:15:29 +0900

    [Bronze IV] Title: 강당 대관, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/31994. 강당 대관/강당 대관.py
+++ 백준/Bronze/31994. 강당 대관/강당 대관.py
@@ -1,15 +1,2 @@
-d = []
-max_ = 0
-winner = ""
-
-for i in range(7):
-    s = input().split()
-    d.append(s)
-    d[i][1] = int(d[i][1])
-    if d[i][1] > max_:
-        max_ = d[i][1]
-        winner = d[i][0]
-    #print(max_, winner)
-
-#print(d)
-print(winner)
+d = {s: int(n) for s, n in (input().split() for _ in range(7))}
+print(max(d, key=d.get))
